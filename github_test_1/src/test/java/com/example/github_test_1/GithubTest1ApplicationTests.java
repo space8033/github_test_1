8fec17b99b1dea299f4fa6bc54672adff82f0da6
space8033 <space8237@naver.com>
2022-08-22 18:44:56 +0900

    initial

---
+++ github_test_1/src/test/java/com/example/github_test_1/GithubTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_test_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
